690e81ee4b5bceb27526d06ea5b5e6fc702b5e86
alierkanimrek <alierkanimrek@gmail.com>
2024-06-27 17:28:29 +0300

    Script updates

--- install.sh
+++ install.sh
@@ -2,30 +2,30 @@
 echo "There are users into system"
 users
 echo "Installation for user '$1'"
-read -p "is it ok?"
+read -p "is it ok?" x
 echo "Did you change username in service script"
 cat nk2/nk2.service
-read -p "Ready for apt installations..."
+read -p "Ready for apt installations..." x
 apt-get update
 apt install git apt-transport-https gpg
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg  --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' \
   | tee /etc/apt/sources.list.d/dart_stable.list
 apt-get update && apt-get install dart
-read -p "Ready for download..."
+read -p "Ready for download..." x
 rm -rf nk2
 git clone https://github.com/alierkanimrek/nk2.git
-read -p "Ready for install to /opt/nk2"
+read -p "Ready for install to /opt/nk2" x
 mkdir /opt/nk2
 cp nk2/* /opt/nk2 -r
 chown $1:$1 /opt/nk2 -R
-read -p "Ready for get dart packages..."
+read -p "Ready for get dart packages..." x
 su - $1 -c "cd /opt/nk2; dart pub get"
-read -p "Ready for configuration..."
+read -p "Ready for configuration..." x
 mkdir /etc/nk2
 cp nk2/bin/config.json.template /etc/nk2/config.json
 nano /etc/nk2/config.json
-read -p "Ready for service installation..."
+read -p "Ready for service installation..." x
 cp -v nk2/nk2.service /lib/systemd/system/nk2.service
 systemctl daemon-reload
 systemctl enable nk2
@@ -33,10 +33,10 @@ systemctl start nk2
 sleep 3
 systemctl status nk2
 echo "wget -c https://raw.githubusercontent.com/alierkanimrek/nk2/main/update.sh">downupdate.sh
-echo "sh update.sh">>downupdate.sh
+echo "bash update.sh">>downupdate.sh
 chmod +x downupdate.sh
 ls -l
-read -p "just run downupdate.sh for updating"
+read -p "just run downupdate.sh for updating" x
 
 
 
--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 user=$(ls /opt/nk2 -l | grep nk2 | cut -f 3 -d " ")
 echo "Updating for user '$user'"
-read -p "is it ok?"
-read -p "Ready for downloading..."
+read -p "is it ok?" x
+read -p "Ready for downloading..." x
 rm -rf nk2
 git clone https://github.com/alierkanimrek/nk2.git
-read -p "Ready for stop service and update..."
+read -p "Ready for stop service and update..." x
 systemctl stop nk2
 cp nk2/* /opt/nk2 -r
 chown $user:$user /opt/nk2 -R
