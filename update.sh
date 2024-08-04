5b6f694f6dbe728cc408e70c0bd426cbd279a7a3
alierkanimrek <alierkanimrek@gmail.com>
2024-08-04 17:22:17 +0300

    Jsony update changes on config.json

--- update.sh
+++ update.sh
@@ -9,6 +9,12 @@ read -p "Ready for stop service and update..." x
 systemctl stop nk2
 cp nk2/* /opt/nk2 -r
 chown $user:$user /opt/nk2 -R
+# ----- Config update
+mv /etc/nk2/config.json /etc/nk2/config.old
+echo -e "{\n \"nk\":" > /etc/nk2/config.json
+cat /etc/nk2/config.old >> /etc/nk2/config.json
+echo -e "\n}" >> /etc/nk2/config.json
+# ----- remove after update
 systemctl start nk2
 sleep 3
 systemctl status nk2
